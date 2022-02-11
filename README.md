41b0a79cc0e8fec9d4d985a0849b497f3e8317d7
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2022-02-11 01:32:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Repositório de algoritmos básicos em Python
+
+Repositório de algoritimos do curso de **ADS** da Fatec. Aqui, colocarei 
+alguns dos algoritimos das aulas de **Algoritimos e Lógica de Programação.**
+
+Seja bem-vindos, e abra seu VS Code.
---
+++ src/basic/min.py
@@ -0,0 +1,17 @@
+# function to get min num of integer array
+def get_min(array):
+    var1 = 0
+    for i in range(0, len(array)):
+        if i == 0:
+            var1 = array[i]
+        else:
+            if array[i] <= var1:
+                var1 = array[i]
+    return var1
+
+
+list_num = [23, 44, 55, 9, 235, 90, 1, 3, 4, 5, 6, -1]
+
+
+# Return -1, because its the smallest value in the list
+print(f'The min value of array is: {get_min(list_num)}')
